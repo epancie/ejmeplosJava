b47fccb4700ff8cd4b53e377cc459bbf956a2712
Pancho Panciera <Pancho Panciera>
2016-03-13 20:32:26 -0300

    Ejemplos Colecciones en java

---
+++ src/Colecciones/Principal.java
@@ -0,0 +1,24 @@
+package Colecciones;
+
+import java.util.ArrayList;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList <String> versionesAndroids = new ArrayList<String>();
+		versionesAndroids.add("Cupcake");
+		versionesAndroids.add("Donut");
+		versionesAndroids.add("Eclair");
+		versionesAndroids.add("Froya");
+		versionesAndroids.add("Gingerbread");
+		versionesAndroids.add("Honeycomb");
+		versionesAndroids.add("Ice cream Sandwich");
+		versionesAndroids.add("Jelly Bean");
+		versionesAndroids.add("Kit Kat");
+		versionesAndroids.add("Lollipop");
+		versionesAndroids.add("Marshmollow");
+	
+	}
+
+}
